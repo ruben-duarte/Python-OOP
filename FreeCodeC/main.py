29931235905a1b6c1428c25f4d3f5a62a43918ed
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2023-05-09 11:27:41 -0500

    study2

--- FreeCodeC/main.py
+++ FreeCodeC/main.py
@@ -1,31 +1,39 @@
 class Item:
-   pay_rate = 0.8 #20% discount rate
+   pay_rate = 0.8 #20% discount rate // class attribute
+   all = []
 
    def __init__(self,name: str, price: float , quantity=0):
         #Run validations to the received arguments
         assert price >= 0, f"price {price} is negative"
         assert quantity >= 0, f"quantity {quantity} is not greater or equal to zero" 
 
-
         #assign to the self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        #actions to execute
+        Item.all.append(self)
+
 
    def total_price(self):
         return self.price*self.quantity
+   
+
+   def apply_discount(self):
+       self.price = self.price * self.pay_rate
 
 
-item_1 = Item('bike', 45)    
-item_2 = Item('phone', 350, 2)
-item_2.has_ear_buds = False
+   def __repr__(self):
+       return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-print(item_1.name)
-print(item_2.name)
-print(item_1.price)
-print(item_2.price)
-print(item_1.quantity)
-print(item_2.quantity)
-print(item_1.total_price())
-print(Item.__dict__)
+
+item1 = Item("Phone",100, 1)
+item2 = Item("Laptop",1000, 3)
+item3 = Item("Cable",10, 5)
+item4 = Item("Mouse",50, 5)
+item5 = Item("Keyboard",75, 5)
+
+print(Item.all)
+for instance in Item.all:
+    print(instance.name)
--- play/canvas.py
+++ play/canvas.py
@@ -1,37 +1,61 @@
+class Rectangle:
 
-def  draw_rect(width=0, height=0):
-    print()
-    for row in range(height):
-        for col in range(width):
-            if (row == 0  or row == height - 1) or ((col == 0 or col == width - 1) and (row != 0 or row != height - 1)):
-                print("=", end=' ')
-            else:
-                print(" ",end= ' ')
+    def __init__(self,width=0,height=0):
+        self.width = width
+        self.height = height
+
+    def  draw_rect(self):
         print()
+        for row in range(self.height):
+            for col in range(self.width):
+                if (row == 0  or row == self.height - 1) or ((col == 0 or col == self.width - 1) and (row != 0 or row != self.height - 1)):
+                    print("=", end=' ')
+                else:
+                    print(" ",end= ' ')
+            print()
 
-draw_rect(14,8)
-draw_rect(10,8)
-draw_rect(6,8 )
 
 
-def  draw_rect_with_text(height=0, text=' Hello!'):
-    print()
-    width = len(text) + 4
-    for row in range(height):
-        for col in range(width):
-            if (row == 0  or row == height - 1) or ((col == 0 or col == width - 1) and (row != 0 or row != height - 1)):
-                print("=", end='')
-            elif row == height//2:
-                print(" " , text, " ", "=")
-                break
-            else:
-                print(" ",end= ' ')
-        print()
+rect_1 = Rectangle(4,8)
+rect_2 = Rectangle(2,4)
+rect_3 = Rectangle(5,10)
 
-text ='Wordle game Welcome! choose an option'
-text2 = """
-Bienvenidos 
-          al sistema de usuario
-                en la terminal  ✨
-"""
-draw_rect_with_text(3, text)
+print(rect_1.draw_rect())
+print(rect_2.draw_rect())
+print(rect_3.draw_rect())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def  draw_rect_with_text(height=0, text=' Hello!'):
+#     print()
+#     width = len(text) + 4
+#     for row in range(height):
+#         for col in range(width):
+#             if (row == 0  or row == height - 1) or ((col == 0 or col == width - 1) and (row != 0 or row != height - 1)):
+#                 print("=", end='')
+#             elif row == height//2:
+#                 print(" " , text, " ", "=")
+#                 break
+#             else:
+#                 print(" ",end= ' ')
+#         print()
+
+# text ='Wordle game Welcome! choose an option'
+# text2 = """
+# Bienvenidos 
+#           al sistema de usuario
+#                 en la terminal  ✨
+# """
+# draw_rect_with_text(3, text)
